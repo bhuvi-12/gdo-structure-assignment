a2c11ff75c56f147e3d1f5883e75c7526289d65d
Bhuvan Prasad G <bhuvanprasadg500@gmail.com>
2022-06-08 14:55:09 +0530

    wednesday commit

--- dao/goals.js
+++ dao/goals.js
@@ -3,41 +3,100 @@ const Users = require("../models/users");
 const Op = require("sequelize").Op;
 const sequelize = require("sequelize");
 
-async function getAllGoalsOfUser(id,role,month) {
-  Users.belongsTo(Goals, {targetKey:'user_id', foreignKey:'id'});
+async function getAllGoalsOfUser(id, role, month) {
+  Users.belongsTo(Goals, { targetKey: "user_id", foreignKey: "id" });
   return Users.findAll({
-    where:{
-      role:role,
-      id:id,
+    where: {
+      role: role,
+      id: id,
     },
     include: {
       model: Goals,
-      required:true,
+      required: true,
       where: {
-        [Op.and] : [
-           sequelize.fn('EXTRACT(MONTH from "date") =', month)
-        ]
-      }
+        [Op.and]: [sequelize.fn('EXTRACT(MONTH from "date") =', month)],
+      },
     },
   });
 }
 
-async function addGoalsofEmployee({goal_name,status,date,user_id,createdAt,updatedAt}){
-  return Goals.create({goal_name,status,date,user_id,createdAt,updatedAt});
+async function addGoalsofEmployee({
+  goal_name,
+  status,
+  date,
+  user_id,
+  createdAt,
+  updatedAt,
+}) {
+  return Goals.create({
+    goal_name,
+    status,
+    date,
+    user_id,
+    createdAt,
+    updatedAt,
+  });
 }
 
-async function addGoalsofAdmin({goal_name,status,date,user_id,createdAt,updatedAt}){
-  return Goals.create({goal_name,status,date,user_id,createdAt,updatedAt});
+async function addGoalsofAdmin({
+  goal_name,
+  status,
+  date,
+  user_id,
+  createdAt,
+  updatedAt,
+}) {
+  return Goals.create({
+    goal_name,
+    status,
+    date,
+    user_id,
+    createdAt,
+    updatedAt,
+  });
 }
 
-async function addGoalsofSuperAdmin({goal_name,status,date,user_id,createdAt,updatedAt}){
-  return Goals.create({goal_name,status,date,user_id,createdAt,updatedAt});
+async function addGoalsofSuperAdmin({
+  goal_name,
+  status,
+  date,
+  user_id,
+  createdAt,
+  updatedAt,
+}) {
+  return Goals.create({
+    goal_name,
+    status,
+    date,
+    user_id,
+    createdAt,
+    updatedAt,
+  });
 }
 
-async function deleteGoal(id){
+async function deleteGoal(id) {
   return Goals.destroy({
     where: { id: id },
   });
 }
 
-module.exports = { getAllGoalsOfUser, addGoalsofEmployee, addGoalsofAdmin, addGoalsofSuperAdmin, deleteGoal };
+async function updateGoal({ id, goal_name, status, updatedAt }) {
+  return Goals.update({
+    goal_name:goal_name,
+    status:status,
+    updatedAt:updatedAt,
+  },{
+    where:{
+      id:id
+    }
+  });
+}
+
+module.exports = {
+  getAllGoalsOfUser,
+  addGoalsofEmployee,
+  addGoalsofAdmin,
+  addGoalsofSuperAdmin,
+  deleteGoal,
+  updateGoal
+};
--- goals.js
+++ goals.js
@@ -1,77 +1,92 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
 const goalsDao = require("./dao/goals");
 
-router.get('/goals', jsonParser, async(req, res) => {
-    try{
-        res.json({
-            data: await goalsDao.getAllGoalsOfUser(req.query.id,req.query.role,req.query.month),
-        });
-    } catch(err){
-        res.json({
-            error: err.toString(),
-        })
-    }
+router.get("/goals", jsonParser, async (req, res) => {
+  try {
+    res.json({
+      data: await goalsDao.getAllGoalsOfUser(
+        req.query.id,
+        req.query.role,
+        req.query.month
+      ),
+    });
+  } catch (err) {
+    res.json({
+      error: err.toString(),
+    });
+  }
 });
 
-router.post('/employee-goals', jsonParser, async(req, res) => {
-    console.log(req.body);
-    try{
-        const newGoals = await goalsDao.addGoalsofEmployee(req.body)
-        res.json({
-            message: "goals inserted successfully",
-            data: newGoals
-        });
-    } catch(err){
-        res.json({
-            error: err.toString(),
-        })
-    }
+router.post("/employee-goals", jsonParser, async (req, res) => {
+  try {
+    const newGoals = await goalsDao.addGoalsofEmployee(req.body);
+    res.json({
+      message: "goals inserted successfully",
+      data: newGoals,
+    });
+  } catch (err) {
+    res.json({
+      error: err.toString(),
+    });
+  }
 });
 
-router.post('/admin-goals', jsonParser, async(req, res) => {
-    console.log(req.body);
-    try{
-        const newGoals = await goalsDao.addGoalsofAdmin(req.body)
-        res.json({
-            message: "goals inserted successfully",
-            data: newGoals
-        });
-    } catch(err){
-        res.json({
-            error: err.toString(),
-        })
-    }
+router.post("/admin-goals", jsonParser, async (req, res) => {
+  try {
+    const newGoals = await goalsDao.addGoalsofAdmin(req.body);
+    res.json({
+      message: "goals inserted successfully",
+      data: newGoals,
+    });
+  } catch (err) {
+    res.json({
+      error: err.toString(),
+    });
+  }
 });
 
-router.post('/super_admin-goals', jsonParser, async(req, res) => {
-    try{
-        const newGoals = await goalsDao.addGoalsofSuperAdmin(req.body)
-        res.json({
-            message: "goals inserted successfully",
-            data: newGoals
-        });
-    } catch(err){
-        res.json({
-            error: err.toString(),
-        })
-    }
+router.post("/super_admin-goals", jsonParser, async (req, res) => {
+  try {
+    const newGoals = await goalsDao.addGoalsofSuperAdmin(req.body);
+    res.json({
+      message: "goals inserted successfully",
+      data: newGoals,
+    });
+  } catch (err) {
+    res.json({
+      error: err.toString(),
+    });
+  }
 });
 
-router.delete('/delete', async(req,res) => {
-    console.log(req.query.id);
-    try{
-        await goalsDao.deleteGoal(req.query.id);
-        res.json({
-            message: "goal deleted successfully",
-        })
-    }catch(err){
-        res.json({
-            error: err.toString(),
-        })
-    }
-})
+router.put("/update", jsonParser,async (req, res) => {
+  console.log(req.body);
+  try {
+    await goalsDao.updateGoal(req.body);
+    res.json({
+      message: "goals updated successfully",
+    });
+  } catch (err) {
+    res.json({
+      error: err.toString(),
+    });
+  }
+});
+
+router.delete("/delete", async (req, res) => {
+  try {
+    await goalsDao.deleteGoal(req.query.id);
+    res.json({
+      message: "goal deleted successfully",
+    });
+  } catch (err) {
+    res.json({
+      error: err.toString(),
+    });
+  }
+});
 
 module.exports = router;
